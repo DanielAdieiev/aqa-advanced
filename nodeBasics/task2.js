1b55f9436b65b504b7b717d406b99ef4049f4d10
Adieiev Daniel <danieladieiev@gmail.com>
2025-01-20 17:10:16 +0200

    fixed comments

--- nodeBasics/task2.js
+++ nodeBasics/task2.js
@@ -2,6 +2,5 @@ let fullName1 = "John Doe";
 let fullName2 = "Jane Doe";
 let greetBoth = "Hello, " + fullName1 + " and " + fullName2 + "!";
 console.log(greetBoth);
-let greetOne = "Hello, ";
-console.log (greetOne + fullName1 + "!");
-console.log (greetOne + fullName2 + "!");
+let greetSample = `Hello, ${fullName1} and ${fullName2}!`;
+console.log(greetSample);
--- nodeBasics/task3.js
+++ nodeBasics/task3.js
@@ -1,4 +1,4 @@
 let age = 73;
-let isAdult = age >= 18 ? true : false;
+let isAdult = age >= 18 ? 'Adult' : 'Young';
 console.log(age);
 console.log(isAdult);
