98d519aecfccb1cbcf8576ce846709f58a7e450d
DanielAdieiev <103453967+DanielAdieiev@users.noreply.github.com>
2025-01-20 20:16:16 +0200

    Update task3.js

--- homeWork5/task3.js
+++ homeWork5/task3.js
@@ -2,7 +2,7 @@ let selectedNumber = 5;
 for (let i=1; i<=10; i++){
     console.log(`${selectedNumber} * ${i} = ${selectedNumber*i}`);
 };
-let i=0;
+let i=1;
 while (i <= 10){
     console.log(`${selectedNumber} * ${i} = ` + selectedNumber*i);
     i++;
