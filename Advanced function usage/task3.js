41f0079bb3eb443a6ac216dff201949d4a1df3d7
DanielAdieiev <103453967+DanielAdieiev@users.noreply.github.com>
2025-01-31 17:08:32 +0200

    Update task3.js
    added spaces

--- Advanced function usage/task3.js
+++ Advanced function usage/task3.js
@@ -1,49 +1,49 @@
 function divide (numerator, denominator){
     if (denominator === 0 || denominator === null) {
         throw new Error ("Cannot divide by zero");
-    } else if (isNaN(numerator) || isNaN(denominator)) {
+    } else if (isNaN (numerator) || isNaN (denominator)) {
         throw new Error ("Use numbers only");
     } else {
-        console.log(numerator / denominator);  
+        console.log (numerator / denominator);  
     }
 }
 
 try {
-    divide ( 1, null);
+    divide (1, null);
 } catch (error){
-    console.log(error.message);
+    console.log (error.message);
 } finally {
-    console.log("Робота завершена");
+    console.log ("Робота завершена");
 }
 
 try {
-    divide ( 1, 0);
+    divide (1, 0);
 } catch (error){
-    console.log(error.message);
+    console.log (error.message);
 } finally {
-    console.log("Робота завершена");
+    console.log ("Робота завершена");
 }
 
 try {
     divide (5, 2);
 } catch (error){
-    console.log(error.message);
+    console.log (error.message);
 } finally {
-    console.log("Робота завершена");
+    console.log ("Робота завершена");
 }
 
 try {
     divide ("2", 5);
 } catch (error){
-    console.log(error.message);
+    console.log (error.message);
 } finally {
-    console.log("Робота завершена");
+    console.log ("Робота завершена");
 }
 
 try {
     divide (5, "2wrrrrr");
 } catch (error){
-    console.log(error.message);
+    console.log (error.message);
 } finally {
-    console.log("Робота завершена");
+    console.log ("Робота завершена");
 }
