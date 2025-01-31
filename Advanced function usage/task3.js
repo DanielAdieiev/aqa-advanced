979f3fadc093f763f8e9c51e8ec6fad4ceb61922
Adieiev Daniel <danieladieiev@gmail.com>
2025-01-31 13:09:52 +0200

    hw7.1 done

--- Advanced function usage/task3.js
+++ Advanced function usage/task3.js
@@ -1,13 +1,37 @@
 function divide (numerator, denominator){
     if (denominator === 0){
-        console.log ("Cannot divide by zero");
+        throw new Error ("Cannot divide by zero");
     } else if (isNaN(numerator) || isNaN(denominator)) {
-        console.log ("Use numbers only");
+        throw new Error ("Use numbers only");
     } else {
         console.log(numerator / denominator);  
     }
 }
-divide ( 1, 0);
-divide (5, 2);
-divide ("2", 5);
-divide (5, "2wrrrrr");
+
+try {divide ( 1, 0);
+} catch (error){
+    console.log(error.message);
+} finally {
+    console.log("Робота завершена");
+}
+
+try {divide (5, 2);
+} catch (error){
+    console.log(error.message);
+} finally {
+    console.log("Робота завершена");
+}
+
+try {divide ("2", 5);
+} catch (error){
+    console.log(error.message);
+} finally {
+    console.log("Робота завершена");
+}
+
+try {divide (5, "2wrrrrr");
+} catch (error){
+    console.log(error.message);
+} finally {
+    console.log("Робота завершена");
+}
